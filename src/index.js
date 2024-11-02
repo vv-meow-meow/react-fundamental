235f6eb500caff4593a650699e40d709a3b3eb9b
vv-meow-meow <181574907+vv-meow-meow@users.noreply.github.com>
2024-11-03 01:07:47 +0300

    Create root render with jsx

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(React.createElement("button", {
-  onClick: () => console.log('clicked')
-}, "Нажми на меня!"));
+root.render(// <App />
+    <div>
+      Приложение работает
+      <button>Кнопка</button>
+    </div>);
