c87c1a8dd2cecc1eb2b53e4e6a3c0f6047528b60
vv-meow-meow <181574907+vv-meow-meow@users.noreply.github.com>
2024-11-03 01:11:46 +0300

    Update increment function (doesn't work)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 function App() {
-  let likes = 0
+  let likes = 5;
+
+  function increment() {
+    likes += 1;
+    console.log(likes);
+  }
+
   return (<div className="App">
     <h1>{likes}</h1>
-    <button onClick={() => likes += 1}>Increment</button>
+    <button onClick={increment}>Increment</button>
     <button onClick={() => likes -= 1}>Decrement</button>
   </div>);
 }
