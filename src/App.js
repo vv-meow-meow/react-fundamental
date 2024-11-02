ebf1480cc6e5e8687f16831f14383b959ab43461
vv-meow-meow <181574907+vv-meow-meow@users.noreply.github.com>
2024-11-03 01:09:25 +0300

    Create increment/decrement logic (doesn't work)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 function App() {
-  return (
-      <div className="App">
-      </div>
-  );
+  let likes = 0
+  return (<div className="App">
+    <h1>{likes}</h1>
+    <button onClick={() => likes += 1}>Increment</button>
+    <button onClick={() => likes -= 1}>Decrement</button>
+  </div>);
 }
 
 export default App;
