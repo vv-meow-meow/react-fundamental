dd8f51c4c3db023f0ad3a42d0b6daea330d9c6e0
vv-meow-meow <181574907+vv-meow-meow@users.noreply.github.com>
2024-11-03 21:10:54 +0300

    Add styles, props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, {useState} from "react";
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
+import "./styles/App.css"
+import PostItem from "./components/PostItem";
 
 function App() {
-  const [likes, setLikes] = useState(5);
-  const [value, setValue] = useState("Text inside the input")
 
   return (<div className="App">
-    <ClassCounter/>
+    <PostItem post={{id: 1, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 2, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 3, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 4, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 5, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 5, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 5, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 5, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 5, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 5, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 5, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 5, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 5, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 5, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 5, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 5, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 5, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 5, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 5, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 5, title: "Javascript", body: "Description"}}/>
+    <PostItem post={{id: 5, title: "Javascript", body: "Description"}}/>
   </div>);
 }
 
