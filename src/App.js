dd6f68f20fdc41754dd464952c19ea15b0317da7
vv-meow-meow <181574907+vv-meow-meow@users.noreply.github.com>
2024-11-03 20:33:10 +0300

    Create ClassComponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {useState} from "react";
 import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
   const [likes, setLikes] = useState(5);
   const [value, setValue] = useState("Text inside the input")
 
   return (<div className="App">
-    <Counter/>
+    <ClassCounter/>
   </div>);
 }
 
