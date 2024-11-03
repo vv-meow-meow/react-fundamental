9f3cbeb9ae7f4764c06195d9d74a985344d10493
vv-meow-meow <181574907+vv-meow-meow@users.noreply.github.com>
2024-11-03 19:33:58 +0300

    Create managed input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
 
 function App() {
-  const [likes, setLikes] = useState(0);
+  const [likes, setLikes] = useState(5);
+  const [value, setValue] = useState("Text inside the input")
 
   function increment() {
     setLikes(likes + 1);
@@ -13,9 +14,17 @@ function App() {
 
   return (<div className="App">
     <h1>{likes}</h1>
+    <h1>{value}</h1>
+    <input
+        type="text"
+        value={value}
+        onChange={event => setValue(event.target.value)}
+    />
     <button onClick={increment}>Increment</button>
     <button onClick={decrement}>Decrement</button>
   </div>);
 }
 
 export default App;
+
+// 22:40
