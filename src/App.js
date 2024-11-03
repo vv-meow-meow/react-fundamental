dd8588e1fbcbab422701e5793b1fc98549279a6d
vv-meow-meow <181574907+vv-meow-meow@users.noreply.github.com>
2024-11-03 22:15:41 +0300

    Create UI library, CSS modules

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
 import './styles/App.css';
-import PostItem from './components/PostItem';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -10,15 +11,26 @@ function App() {
     {id: 3, title: 'HTML', body: 'Description'},
     {id: 4, title: 'CSS', body: 'Description'}]);
 
-  const [posts2, setPosts2] = useState([
-    {id: 1, title: 'Python', body: 'Description'},
-    {id: 2, title: 'AIOgram', body: 'Description'},
-    {id: 3, title: 'SQLAlchemy', body: 'Description'},
-    {id: 4, title: 'Fluentogram', body: 'Description'}]);
+  const [title, setTitle] = useState('');
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title);
+  };
 
   return (<div className="App">
+    <form>
+      {/*Управляемый компонент*/}
+      <MyInput
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="Название поста"/>
+      <MyInput
+          type="text"
+          placeholder="Описание поста"/>
+      <MyButton onClick={addNewPost}>Создать пост</MyButton>
+    </form>
     <PostList posts={posts} title="Посты про JS"/>
-    <PostList posts={posts2} title="Посты про Python"/>
   </div>);
 }
 
