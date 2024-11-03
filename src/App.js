6c4d6fb11960ae5251a14c31fb9b3ff8696db1c3
vv-meow-meow <181574907+vv-meow-meow@users.noreply.github.com>
2024-11-04 00:25:56 +0300

    Learn unmanaged component, useRef() hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
@@ -12,20 +12,25 @@ function App() {
     {id: 4, title: 'CSS', body: 'Description'}]);
 
   const [title, setTitle] = useState('');
+  const bodyInputRef = useRef();
+
   const addNewPost = (e) => {
     e.preventDefault();
     console.log(title);
+    console.log(bodyInputRef.current.value);
   };
 
   return (<div className="App">
     <form>
-      {/*Управляемый компонент*/}
+      {/* Managed component */}
       <MyInput
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           type="text"
           placeholder="Название поста"/>
+      {/* Unmanaged / Uncontrolled Component */}
       <MyInput
+          ref={bodyInputRef}
           type="text"
           placeholder="Описание поста"/>
       <MyButton onClick={addNewPost}>Создать пост</MyButton>
