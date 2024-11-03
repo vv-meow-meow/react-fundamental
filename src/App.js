9f2ead3dc214e22a825136f58c48aed620e9d11c
vv-meow-meow <181574907+vv-meow-meow@users.noreply.github.com>
2024-11-04 00:46:31 +0300

    Wrap form values into an object (not two strings variables)

--- src/App.js
+++ src/App.js
@@ -11,29 +11,24 @@ function App() {
     {id: 3, title: 'HTML', body: 'Description'},
     {id: 4, title: 'CSS', body: 'Description'}]);
 
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
+  const [post, setPost] = useState({title: '', body: ''});
 
   const addNewPost = (e) => {
     e.preventDefault();
-    const newPost = {
-      id: Date.now(), title, body,
-    };
-    setPosts([...posts, newPost]);
-    setTitle('');
-    setBody('');
+    setPosts([...posts, {...post, id: Date.now()}]);
+    setPost({title: '', body: ''});
   };
 
   return (<div className="App">
     <form>
       <MyInput
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          value={post.title}
+          onChange={(e) => setPost({...post, title: e.target.value})}
           type="text"
           placeholder="Название поста"/>
       <MyInput
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
+          value={post.body}
+          onChange={(e) => setPost({...post, body: e.target.value})}
           type="text"
           placeholder="Описание поста"/>
       <MyButton onClick={addNewPost}>Создать пост</MyButton>
