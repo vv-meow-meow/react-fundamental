eb6d25518b505834ad8015df58effad65ab1d20f
vv-meow-meow <181574907+vv-meow-meow@users.noreply.github.com>
2024-11-06 10:04:08 +0300

    Create modal window

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './styles/App.css';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
 import PostFilter from './components/PostFilter';
+import MyModal from './components/UI/MyModal/MyModal';
+import MyButton from './components/UI/button/MyButton';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -12,6 +14,7 @@ function App() {
     {id: 4, title: 'CSS', body: 'zzzz'}]);
 
   const [filter, setFilter] = useState({sort: '', query: ''});
+  const [modal, setModal] = useState(false);
 
   const sortedPosts = useMemo(() => {
     console.log('sortedPosts call');
@@ -29,6 +32,7 @@ function App() {
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
+    setModal(false);
   };
 
   const removePost = (post) => {
@@ -36,7 +40,12 @@ function App() {
   };
 
   return (<div className="App">
-    <PostForm create={createPost}/>
+    <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+      Create post
+    </MyButton>
+    <MyModal visible={modal} setVisible={setModal}>
+      <PostForm create={createPost}/>
+    </MyModal>
     <hr style={{margin: '15px 0'}}/>
     <PostFilter filter={filter} setFilter={setFilter}/>
     <PostList remove={removePost} posts={sortedAndSearchedPosts}
