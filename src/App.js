2d8f7f226e7b9d92c23de1842300b4fda4ff9923
vv-meow-meow <181574907+vv-meow-meow@users.noreply.github.com>
2024-11-08 00:10:45 +0300

    Custom hook useFetching, error handling

--- src/App.js
+++ src/App.js
@@ -8,13 +8,18 @@ import MyButton from './components/UI/button/MyButton';
 import {usePosts} from './hooks/usePosts';
 import PostService from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
+import {useFetching} from './hooks/useFetching';
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostsLoading, setIsPostsLoading] = useState(false);
+
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
 
   useEffect(() => {
     fetchPosts();
@@ -24,15 +29,6 @@ function App() {
     setPosts([...posts, newPost]);
     setModal(false);
   };
-
-  async function fetchPosts() {
-    setIsPostsLoading(true);
-    setTimeout(async () => {
-      const posts = await PostService.getAll();
-      setPosts(posts);
-      setIsPostsLoading(false);
-    }, 1000);
-  }
 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id));
@@ -48,6 +44,7 @@ function App() {
     </MyModal>
     <hr style={{margin: '15px 0'}}/>
     <PostFilter filter={filter} setFilter={setFilter}/>
+    {postError && <h1>An error occurred: {postError}</h1>}
     {isPostsLoading ? <div
         style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}>
       <Loader/>
